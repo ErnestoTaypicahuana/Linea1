6893e142446ba1d2b64e198678d3c9fc88774526
Ernesto <etaypicahuanal@uni.pe>
2019-08-16 14:47:28 -0500

    Proyecto1.2

--- process.py
+++ process.py
@@ -4,7 +4,7 @@ import glob, os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Directory where the data will reside, relative to 'darknet.exe'
-path_data = 'data/Books/'
+path_data = 'data/People/'
 
 # Percentage of images to be used for the test set
 percentage_test = 20;
